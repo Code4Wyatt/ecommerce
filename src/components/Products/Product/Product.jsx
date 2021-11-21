a071afe7d5ada5926efb2166d78a4c3eb298f102
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-21 22:16:57 +0000

    update

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton, Icon } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
 import useStyles from './styles';
 
 const Product = ({ product }) => {
     const classes = useStyles();
+
+    console.log("products:", product);
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from './styles';
 
-const products = [
-  {
-    id: 1,
-    name: "Example Product",
-    image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    description: "Product Description Example",
-    price: "£10",
-  },
-  {
-    id: 2,
-    name: "2nd Example Product",
-    image:
-      'https://cdn.vox-cdn.com/thumbor/3o5bkD-T3oQ3EIfXotA4k9P97TY=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/22443013/5.png',
-    description: "2nd Product Description Example",
-    price: "£20",
-  },
-];
 
-const Products = () => {
+const Products = ({ products }) => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
