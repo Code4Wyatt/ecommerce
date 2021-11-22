c15b1d5272be3caca1fc9a8043260a8b922a5a45
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-22 12:02:14 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { commerce } from '../src/lib/commerce';
-import { Products, Navbar, Cart, Checkout } from './components';
-
+import { commerce } from "../src/lib/commerce";
+import { Products, Navbar, Cart, Checkout } from "./components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
-  
-  const fetchProducts = async () => {
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+ const fetchProducts = async () => {
     const { data } = await commerce.products.list();
 
     setProducts(data);
-  }
+  };
 
   const fetchCart = async () => {
     setCart(await commerce.cart.retrieve());
-  }
-  
+  };
+
   const handleAddToCart = async (productId, quantity) => {
     const item = await commerce.cart.add(productId, quantity);
 
     setCart(item.cart);
-  }
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
 
   useEffect(() => {
     fetchProducts();
@@ -31,11 +70,13 @@ const App = () => {
   console.log(cart);
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items}/>
-      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
-      <Cart cart={cart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+        <Cart cart={cart} />
+      </div>
+    </Router>
   );
 };
 
