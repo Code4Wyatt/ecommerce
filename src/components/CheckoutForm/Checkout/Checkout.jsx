dcc9d59020870555e7e067d0f07411a31a008ff7
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-25 18:35:16 +0000

    Updated address form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -10,6 +10,7 @@ const steps = ['Shipping Address', 'Payment Details'];
 const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
     const classes = useStyles();
 
     useEffect(() => {
@@ -27,6 +28,12 @@ const Checkout = ({ cart }) => {
         }
         generateToken();
     }, [cart])
+
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+    const next = (data) => {
+        setShippingData(data);
+    }
 
     const Confirmation = () => (
         <div>Confirmation</div>
